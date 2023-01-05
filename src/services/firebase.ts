9b27fb9b1c69176b7a54e114c333c6be75937feb
felipeiagocruz <felipeiagomcruz@hotmail.com>
2023-01-05 18:24:13 -0300

    :wrench: chore: exposing firebase config files to github actions deploy the app
    Since this information is harmless to be exposed, it will simplify the acess to this data

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,12 +3,18 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey:
+    import.meta.env.VITE_API_KEY || "AIzaSyA6Z07X1aV5BA_dUflpyvx-aQVPh3tXOH0",
+  authDomain:
+    import.meta.env.VITE_AUTH_DOMAIN ||
+    "gesture-drawing-sketchbook.firebaseapp.com",
+  projectId: import.meta.env.VITE_PROJECT_ID || "gesture-drawing-sketchbook",
+  storageBucket:
+    import.meta.env.VITE_STORAGE_BUCKET ||
+    "gesture-drawing-sketchbook.appspot.com",
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID || "173531431786",
+  appId:
+    import.meta.env.VITE_APP_ID || "1:173531431786:web:6f9fd6e1cc79def5005b13",
 };
 
 const app = initializeApp(firebaseConfig);
