5d98458ff4fdb0790f1c5510f5a73ec166f82808
felipeiagocruz <felipeiagomcruz@hotmail.com>
2023-01-06 17:21:09 -0300

    :package: package: added canvas drawn

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,20 @@ import "./App.css";
 import { AuthContext } from "./contexts/AuthContext";
 import { Link } from "react-router-dom";
 import useAuthChecker from "./hooks/useAuthChecker";
+import CanvasDrawn from "react-canvas-draw";
 
 function App() {
   return (
     <div className="App">
       <Auth />
       <Link to="/collection">Collection</Link>
+
+      <CanvasDrawn brushRadius={3} />
+      <img
+        src={
+          "https://i.pinimg.com/236x/91/2d/6f/912d6f086b9080aba5706fc98ce6e9ba.jpg"
+        }
+      />
     </div>
   );
 }
