c3c9ea8aaeaf04c59be5d55b192ddea5f421c037
felipeiagocruz <felipeiagomcruz@hotmail.com>
2023-01-11 00:08:04 -0300

    :art: style: added some styles to App and root

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
-import { useState, useContext } from "react";
 import Auth from "./components/Auth/Auth";
-import "./App.css";
-import { AuthContext } from "./contexts/AuthContext";
+import classes from "./App.module.css";
+
 import { Link } from "react-router-dom";
 import DrawnWorkbench from "./components/Drawings/DrawnWorkbench";
+import { Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <Auth />
-      <Link to="/collection">Collection</Link>
-      <DrawnWorkbench />
-    </div>
+    <>
+      <div className={classes.div}>
+        <Typography
+          textAlign={"left"}
+          fontSize={"50px"}
+          textTransform={"uppercase"}
+          color={"black"}
+        >
+          Let's
+        </Typography>
+        <Typography
+          textAlign={"left"}
+          fontSize={"50px"}
+          textTransform={"uppercase"}
+          color={"black"}
+        >
+          {" "}
+          try it!
+        </Typography>
+
+        <DrawnWorkbench />
+      </div>
+    </>
   );
 }
 
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,5 +1,13 @@
 import App from "../App";
+import Auth from "../components/Auth/Auth";
+import { Link } from "react-router-dom";
 
 export default function Root() {
-  return <App />;
+  return (
+    <>
+      <Auth />
+      <Link to="/collection">Collection</Link>
+      <App />
+    </>
+  );
 }
