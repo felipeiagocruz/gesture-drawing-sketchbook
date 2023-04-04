d50263a63e0d0ae7f991489188681d9b611babe0
felipeiagocruz <felipeiagomcruz@hotmail.com>
2023-04-04 17:56:29 -0300

    :pencil: text: added introduction and start now button

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from "../src/components/Header/Header";
 import classes from "./App.module.css";
 
 import DrawnWorkbench from "./components/Drawings/DrawnWorkbench";
-import { Typography } from "@mui/material";
+import { Typography, Button } from "@mui/material";
 
 function App() {
   return (
@@ -29,26 +29,63 @@ function App() {
               >
                 Tracing
               </Typography>
+
               <Typography
-                textAlign={"left"}
-                fontSize={"12px"}
-                textTransform={"uppercase"}
+                textAlign={"justify"}
+                lineHeight={1.7}
+                fontSize={"18px"}
                 color={"black"}
               >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
-                non. Numquam doloremque at nesciunt eligendi odio dicta
-                voluptate dolores accusamus unde voluptas, non ratione, maiores
-                consectetur corporis laborum quae delectus? Lorem ipsum dolor
-                sit amet consectetur adipisicing elit. Ipsum, non. Numquam
-                doloremque at nesciunt eligendi odio dicta voluptate dolores
-                accusamus unde voluptas, non ratione, maiores consectetur
-                corporis laborum quae delectus? Lorem ipsum dolor sit amet
-                consectetur adipisicing elit. Ipsum, non. Numquam doloremque at
-                nesciunt eligendi odio dicta voluptate dolores accusamus unde
-                voluptas, non ratione, maiores consectetur corporis laborum quae
-                delectus?
+                The ultimate gesture drawing app for artists and designers!
               </Typography>
+              <Typography
+                textAlign={"justify"}
+                lineHeight={1.7}
+                fontSize={"18px"}
+                color={"black"}
+              >
+                With Gesture Drawing &amp; Tracing, you can easily create
+                dynamic and expressive gesture drawings on your device.
+              </Typography>
+              <Typography
+                textAlign={"justify"}
+                lineHeight={1.7}
+                fontSize={"18px"}
+                color={"black"}
+              >
+                Gesture drawing is a powerful technique used by artists to
+                quickly capture the essence of a subject's movement and form.
+                It's a great way to warm up before a longer drawing session, and
+                it can help you improve your skills in anatomy, proportion, and
+                composition.
+              </Typography>
+
+              <Typography
+                textAlign={"justify"}
+                lineHeight={1.7}
+                fontSize={"18px"}
+                color={"black"}
+              >
+                So if you're an artist looking to improve your gesture drawing
+                skills, or if you simply want to explore a new way of expressing
+                yourself, let's start drawing!
+              </Typography>
+              <div className={classes.buttonStartNow}>
+                <Button
+                  variant="contained"
+                  sx={{
+                    width: 230,
+                    height: 90,
+                    backgroundColor: "#4dbce9",
+                    borderRadius: 50,
+                    fontSize: 22,
+                  }}
+                >
+                  Start now!
+                </Button>
+              </div>
             </div>
+
             <div className={classes.divHeaderImage}></div>
           </div>
 
