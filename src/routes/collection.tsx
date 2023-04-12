715f36450165841549b1bfa9fed22a47532474e6
felipeiagocruz <felipeiagomcruz@hotmail.com>
2023-04-12 19:05:15 -0300

    :art: style: added styles to collection page and header/footer

--- src/routes/collection.tsx
+++ src/routes/collection.tsx
@@ -1,5 +1,10 @@
 import Collection from "../components/Drawings/Collection";
 import Add from "../components/Drawings/Add";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
+import classes from "./collection.module.css";
+
 import { useEffect, useState, useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import useAuthChecker from "../hooks/useAuthChecker";
@@ -11,13 +16,15 @@ export default function CollectionPage() {
     useContext(AuthContext);
 
   return (
-    <div>
+    <div className={classes.div}>
       {isLoggedIn ? (
         <>
           <>
+            <Header />
             <Add />
             <p>{name}</p>
             <Collection />
+            <Footer />
           </>
         </>
       ) : (
